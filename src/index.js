da7c1a1e94da7a5d8d5b510e1f849bb3dc2dd44c
Jason Ge <gejun_1978@yahoo.com>
2018-10-13 07:18:26 +0800

    Try a basic scale function
    Add D3 and try a basic function.
    
    Test: the page renders like before; the console outputs correctly.

--- src/index.js
+++ src/index.js
@@ -6,3 +6,12 @@ document.getElementById("app").innerHTML = `
   for more info about Parcel.
 </div>
 `;
+
+var linearScale = d3
+  .scaleLinear()
+  .domain([0, 600])
+  .range([0, 100])
+  .clamp(true);
+
+console.log(linearScale(25));
+console.log(linearScale(500));
